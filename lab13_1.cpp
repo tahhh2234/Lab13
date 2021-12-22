af89b12daff98335fbc4d05271cbbbf94d17037d
tahhh <thadpongsermsook1@gmail.com>
2021-12-22 14:37:10 +0700

    Lab13_1 completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int a){
+    if(a > 1)
+        a = fibonacci(a - 1) + fibonacci(a -2);
+    return a;
 }
